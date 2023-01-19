ff3746ff7d83381b9c1d86e152e989ebfcee4120
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-01-19 20:20:41 +0545

    updation api

--- backend/models/cattle.model.js
+++ backend/models/cattle.model.js
@@ -7,7 +7,7 @@ const cattleSchema = new mongoose.Schema({
   gender: { type: String, required: true },
   dob: { type: String, required: true },
   cost: { type: String },
-  user_id: { type: String, required: true },
+  user_id: { type: Number, required: true },
 });
 
 // Creating model for cattle
