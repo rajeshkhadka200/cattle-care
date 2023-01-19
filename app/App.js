ae099f9a2d3a193d8d192bd3284e532c0b9dde8b
heckerbl <sarojregmi.official@gmail.com>
2023-01-20 00:32:38 +0545

    FIX: navigation stack error

--- app/App.js
+++ app/App.js
@@ -1,5 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+
+// context
+import Context from "./Context.js";
+
+// external libraries
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// screens
+import HomeScreen from "./screens/HomeScreen";
+import HealthScreen from "./screens/HealthScreen";
+import ExpenceScreen from "./screens/ExpenceScreen";
+import IncomeScreen from "./screens/IncomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterationScreen from "./screens/RegisterationScreen";
 
 export default function App() {
   // creating the custom navigation stack
@@ -9,9 +23,70 @@ export default function App() {
   const [defaultScreen, setDefaultScreen] = useState("HomeScreen");
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to haifdf tah start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Context>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={defaultScreen}>
+          {/* home screen */}
+          <Stack.Screen
+            component={HomeScreen}
+            name="HomeScreen"
+            options={{
+              headerShown: false,
+              animation: "none",
+            }}
+          />
+
+          {/* health screen */}
+          <Stack.Screen
+            component={HealthScreen}
+            name="HealthScreen"
+            options={{
+              headerShown: false,
+              animation: "none",
+            }}
+          />
+
+          {/* Expence screen */}
+          <Stack.Screen
+            component={ExpenceScreen}
+            name="ExpenceScreen"
+            options={{
+              headerShown: false,
+              animation: "none",
+            }}
+          />
+
+          {/* Income screen */}
+          <Stack.Screen
+            component={IncomeScreen}
+            name="IncomeScreen"
+            options={{
+              headerShown: false,
+              animation: "none",
+            }}
+          />
+
+          {/* Login screen */}
+          <Stack.Screen
+            component={LoginScreen}
+            name="LoginScreen"
+            options={{
+              headerShown: false,
+              animation: "none",
+            }}
+          />
+
+          {/* Registeration screen */}
+          <Stack.Screen
+            component={RegisterationScreen}
+            name="RegisterationScreen"
+            options={{
+              headerShown: false,
+              animation: "none",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Context>
   );
 }
